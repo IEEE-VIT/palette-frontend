425886996e15611dd9dc99f3ba541d51837c26f2
ZiyanK <ziyankarmali786@gmail.com>
2020-05-10 01:09:33 +0530

    Divided into router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter,
+  Switch,
+  Route
+} from "react-router-dom";
 import LandingPage from './pages/LandingPage';
+import Dashboard from './pages/Dashboard';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <LandingPage />
-    </div>
+	<div className="App">
+		<BrowserRouter>
+		<Switch>
+			<Route path="/" component={LandingPage} exact={true}/>
+			<Route path="/dashboard" component={Dashboard} exact={true}/>
+		</Switch>
+		</BrowserRouter>
+	</div>
   );
 }
 
