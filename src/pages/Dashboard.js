3a74096bc5e3bbdd900c71d108de1fa4d72db549
ZiyanK <ziyankarmali786@gmail.com>
2020-05-10 15:04:03 +0530

    Set viewport for mobile/desktop

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
+import windowSize from 'react-window-size';
 
 import './Dashboard.css';
+import DashboardMobile from './DashboardMobile/DashboardMobile';
+import DashboardDesktop from './DashboardDesktop/DashboardDesktop';
 
 class Dashboard extends Component {
     render() {
-        return(
-            <div>
-                <h1>Dashboard</h1>
-            </div>
-        )
+        if(this.props.windowWidth < 768) {
+            return(
+                <DashboardMobile />
+            )
+        } else {
+            return(
+                <DashboardDesktop />
+            )
+        }
     }
 }
 
-export default Dashboard;
+export default windowSize(Dashboard);
---
+++ src/pages/DashboardDesktop/DashboardDesktop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './DashboardDesktop.css';
+
+class DashboardDesktop extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>This is the desktop screen</h1>
+            </div>
+        )
+    }
+}
+
+export default DashboardDesktop;
