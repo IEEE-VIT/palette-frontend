c95e04b232b303db5da6a49fedff76cfbb6d727b
ZiyanK <ziyankarmali786@gmail.com>
2020-05-12 23:08:43 +0530

    Pushed time to 1am

--- src/components/RegistrationText/RegistrationText.js
+++ src/components/RegistrationText/RegistrationText.js
@@ -12,7 +12,7 @@ const RegistrationText = () => {
   }
     return(
         <p className="regText">
-            Registrations open in <strong><Countdown end={1589297400} wordsEndingOff={true} text={text} /></strong>
+            Registrations open in <strong><Countdown end={1589311800} wordsEndingOff={true} text={text} /></strong>
         </p>
     )
 }
