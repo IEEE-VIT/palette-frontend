5d23190d210876dd3b0db48b5cf5625f929de3f8
ZiyanK <ziyankarmali786@gmail.com>
2020-05-12 23:13:19 +0530

    Made loading file

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+import './Loading.css';
+
+class Loading extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Loading;
