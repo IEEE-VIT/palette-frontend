09e24dc2b99b7444ee6122dc12908cc8bafe5ca8
ZiyanK <ziyankarmali786@gmail.com>
2020-05-22 12:14:06 +0530

    Ready to push

--- src/DashbaordComponents/Instructions/Instructions.js
+++ src/DashbaordComponents/Instructions/Instructions.js
@@ -15,7 +15,7 @@ const Instructions = () => {
                     <ul> 
                         <li>Your problem statments will be randomly generated. You will be given three tries to generate your problem statement. You can confirm your problem statement on any of the tries but after three tries, the third one will be final. Check out our <a href="https://ieeevit.org/"><strong>Instagram</strong></a> post to see how the generator works.</li>
                         <li>You can not generate new problem statements once you finalise on a problem statement you want to go with. Do not logout until your problem statement has been selected.</li>
-                        <li>To qualify for Round 2, you must join our slack workspace as all <strong> important announcements</strong> will be made there. <a href="https://join.slack.com/t/palette20/shared_invite/zt-eb839cx0-_lTn5ReBAe4N3dFc8tEAEQ"><strong>Click here to join</strong></a> </li>
+                        <li>To qualify for Round 2, you must join our slack workspace as all <strong> important announcements</strong> will be made there. <a href="https://join.slack.com/t/palette20/shared_invite/zt-eb839cx0-_lTn5ReBAe4N3dFc8tEAEQ" className="click-to-join-button"><strong>Click here to join</strong></a> </li>
                         <li>You will have to submit your final prototype at end of round 2 in the submission field provided.</li>
                         <li>For any guidance or queries, kindly ping us on Slack or email to design@ieeevit.org </li>
                     </ul>
