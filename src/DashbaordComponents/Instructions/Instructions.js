444e793aefc1cf66ae92fd2e071827411fd46e2b
ZiyanK <ziyankarmali786@gmail.com>
2020-05-23 09:27:33 +0530

    bold text

--- src/DashbaordComponents/Instructions/Instructions.js
+++ src/DashbaordComponents/Instructions/Instructions.js
@@ -13,7 +13,7 @@ const Instructions = () => {
 
                 <div className="instructions-list">
                     <ul> 
-                        <li>Refresh your page to view the submitted URL link to your wireframe.</li>
+                        <li><strong>Refresh your page to view the submitted URL link to your wireframe.</strong></li>
                         <li>To qualify for Round 2, you must join our slack workspace as all <strong> important announcements</strong> will be made there. <a href="https://join.slack.com/t/palette20/shared_invite/zt-eb839cx0-_lTn5ReBAe4N3dFc8tEAEQ" className="click-to-join-button"><strong>Click here to join</strong></a> </li>
                         <li>You will have to submit your final prototype at end of round 2 in the submission field provided.</li>
                         <li>For any guidance or queries, kindly ping us on Slack or email to design@ieeevit.org </li>
